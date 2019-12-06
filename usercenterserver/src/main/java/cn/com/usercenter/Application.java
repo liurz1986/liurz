b3b0a14aedeb348b8023471d156a65747d68b2ff
liurz <451015469@qq.com>
2019-12-06 22:36:25 +0800

    xx

--- usercenterserver/src/main/java/cn/com/usercenter/Application.java
+++ usercenterserver/src/main/java/cn/com/usercenter/Application.java
@@ -3,38 +3,31 @@ package cn.com.usercenter;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-
 @SpringBootApplication
-//@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @RestController
-public class Application extends SpringBootServletInitializer
-{
+public class Application extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(Application.class);
 	}
 
-	public static void main( String[] args )
-    {
-        SpringApplication.run(Application.class, args);
-    }
-	//https://localhost:8080/usercenter/demo
-	@RequestMapping(value="/demo",method=RequestMethod.GET)
-	public Map<String,String>  test(){
-		Map<String,String> params=new HashMap<String,String>();
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// https://localhost:8080/usercenter/demo
+	@RequestMapping(value = "/demo", method = RequestMethod.GET)
+	public Map<String, String> test() {
+		Map<String, String> params = new HashMap<String, String>();
 		params.put("data", "welcome to you");
 		params.put("status", "success");
+<<<<<<< HEAD
 		params.put（"key","values"）;
+=======
+        params.put("status1", "success");
+>>>>>>> xx
 		return params;
 	}
-	
+
 }
